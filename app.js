dd8bf2a0dc43005cd4b2b3890e72449e58b85c91
Keshaw-Jha <78222085+Keshaw-Jha@users.noreply.github.com>
2023-12-27 00:50:16 +0530

    A better file approach

--- app.js
+++ app.js
@@ -1,78 +1,32 @@
-const fs = require('fs');
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 
+
+// 1)MIDDLEWARES
+app.use(morgan('dev'));
 app.use(express.json());
 
-
-// app.get('/',(req,res)=>{
-//     res
-//     .status(200)
-//     .json({
-//         message:'hello from the server side!',
-//         app:"Natours"
-//     });
-// });
-
-// app.post('/',(req,res)=>{
-//     res.send('You can post to this endpoint....')
-// })
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tourRouter = require('./Routes/tourRoutes');
+const userRouter = require('./Routes/userRoutes');
 
 
-app.get('/api/v1/tours',(req,res)=>{
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours: tours
-        }
+app.use((req, res, next)=>{
+    console.log('Hello from the middleware👋');
+    next();
     });
+
+app.use((req,res, next)=>{
+    req.requestTime = new Date().toISOString();
+    next();
 })
 
-app.get('/api/v1/tours/:id',(req,res)=>{
-    console.log(req.params);
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id)
+// 2)ROUTE HANDLERS
+// 3)ROUTES
+    app.use('/api/v1/tours',tourRouter); 
+    app.use('/api/v1/users',userRouter); 
 
-    if(!tour) return res.status(404).json({
-        "status":'fail',
-        message:'invalid ID',
-    })
-
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours: tour
-        }
-    });
-})
-
-app.post('/api/v1/tours',(req,res)=>{
-    // console.log(req.body);
-
-    const newId = tours[tours.length-1].id + 1;
-    const newTour = Object.assign({id: newId},req.body);
-
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err=>{
-        res.status(201).json({
-            status: 'success',
-            data:{
-                tour: newTour
-            }
-        });
-    })
-})
-
-
-app.patch('/api/v1/tours/:id',(req, res)=>{
-    
-})
-
+//4)START SERVER
 const port = 3000;
 app.listen(port||3000,()=>{
     console.log(`app running on port ${port}...`);
