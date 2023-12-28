e0ae625a477b22917a0f3602e01ad50ce00991e7
Keshaw-Jha <78222085+Keshaw-Jha@users.noreply.github.com>
2023-12-28 05:49:00 +0530

    temp

--- app.js
+++ app.js
@@ -4,29 +4,29 @@ const morgan = require('morgan');
 const app = express();
 
 // 1)MIDDLEWARES
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));//static files access
+app.use(express.static(`${__dirname}/public`)); //static files access
 
-app.use((req, res, next)=>{
-    console.log('Hello from the middleware👋');
-    next();
-    });
+app.use((req, res, next) => {
+  console.log('Hello from the middleware👋');
+  next();
+});
 
-app.use((req,res, next)=>{
-    req.requestTime = new Date().toISOString();
-    next();
-})
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 // IMPORT ROUTES
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // 3)ROUTES
-    app.use('/api/v1/tours',tourRouter); 
-    app.use('/api/v1/users',userRouter); 
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 //4) MADE APP AVAILABLE FOR SERVER
 module.exports = app;
